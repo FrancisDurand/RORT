81cb625eb829352e58a4827187dbb9fc6147683b
cyrilbertrand <90685417+cyrilbertrand@users.noreply.github.com>
2024-03-26 11:17:53 +0100

    squelette

--- decomposition.jl
+++ decomposition.jl
@@ -4,24 +4,30 @@ using Random
 include("master_problem.jl")
 include("esclave.jl")
 
-function decomposition_DW_pickers()
+function decomposition_DW_pickers(N, R, O, q, s, OF, OS, P, Capa, temps_max)
     # Creer la première solution
     v = []
     #Il faut v non vide au début, genre mettre un élément admissible
 
     max_iteration = 100
     iter_number = 0
+    opt = false
 
-    while not opt and iter_number < max_iteration:
+    while !opt && iter_number < max_iteration
         iter_number += 1
 
         # resoudre le probleme maitre
         objectiveValue, sol_lambda, dual_value = master_problem(N, R, O, q, s, OF, OS, P, Capa, temps_max, v)
 
+        println("afficher")
+        println(objectiveValue)
+        println(sol_lambda)
+        println(dual_value)
+
         # resoudre les sous-problemes
-        alpha =
-        beta =
-        nouveau_v = generer_vk(N, R, O, q, s, OF, OS, Capa, P, alpha, beta)
+        #alpha =
+        #beta =
+        #nouveau_v = generer_vk(N, R, O, q, s, OF, OS, Capa, P, alpha, beta)
 
         #calcul coûts réduits ?
 
--- main.jl
+++ main.jl
@@ -3,13 +3,25 @@ using JuMP
 using Random
 include("plne_base.jl")
 include("lecture.jl")
+include("decomposition.jl")
 
 
 function main()
     println("----------------------------------------------------------------------------")
+    #data = parse_data("data/Data_test_N12_R12_O12_RS8.txt")
     data = parse_data("data/Data_test_N5_R4_O3_RS2.txt")
     temps_max = 60
-    objective_sol, x_sol, y_sol = resolution(data.N, data.R, data.O, data.Q, data.S, data.FO, data.SO, data.P, data.Capa, temps_max)
+
+    if false
+        println("test plne base")
+        objective_sol, x_sol, y_sol = resolution(data.N, data.R, data.O, data.Q, data.S, data.FO, data.SO, data.P, data.Capa, temps_max)
+    end
+
+    if true
+        println("test decomposition")
+        decomposition_DW_pickers(data.N, data.R, data.O, data.Q, data.S, data.FO, data.SO, data.P, data.Capa, temps_max)
+    end
+    
 end
 
 main()
